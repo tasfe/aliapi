c7f11c7da3e78f0b7fdba08534eb169ec8d5897a
admin@7895123.com.cn <admin@7895123.com.cn@25adbd58-6b80-11de-8b6a-b7d596239887>
2009-11-10 11:14:37 +0000

    根据新接口更新，取消PID，使用淘宝用户名nick，防止重复，采用变量$usernick

--- toptaobaoke/config.php
+++ toptaobaoke/config.php
@@ -1,6 +1,6 @@
 <?php
-$appKey = '88888888';
-$appSecret = 'fb09877df11205e0904f84f5c0c1ded0';
-$userpid = 'mm_12345678_0_0';
-$usernick = '淘宝用户名';
+//淘宝客填写前请按照此教程操作。http://open.taobao.com/bbs/read.php?tid=3032
+$appKey = '88888888';//填写App Key
+$appSecret = 'fb09877df11205e0904f84f5c0c1ded0';//填写App Secret
+$usernick = '淘宝用户名';//填写淘宝用户名，可以直接填写中文。
 ?>
