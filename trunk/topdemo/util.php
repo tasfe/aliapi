9510aeadc06b3237df8dc81b0a59c9aaa8c4b2d7
admin@7895123.com.cn <admin@7895123.com.cn@25adbd58-6b80-11de-8b6a-b7d596239887>
2010-05-26 03:43:19 +0000

    TOPAPIdemo

---
+++ trunk/topdemo/util.php
@@ -0,0 +1,50 @@
+<?php
+//签名函数
+function createSign ($paramArr) {
+    global $appSecret;
+    $sign = $appSecret;
+    ksort($paramArr);
+    foreach ($paramArr as $key => $val) {
+       if ($key !='' && $val !='') {
+           $sign .= $key.$val;
+       }
+    }
+    $sign = strtoupper(md5($sign));
+    return $sign;
+}
+
+//组参函数
+function createStrParam ($paramArr) {
+    $strParam = '';
+    foreach ($paramArr as $key => $val) {
+       if ($key != '' && $val !='') {
+           $strParam .= $key.'='.urlencode($val).'&';
+       }
+    }
+    return $strParam;
+}
+
+//解析xml函数
+function getXmlData ($strXml) {
+	$pos = strpos($strXml, 'xml');
+	if ($pos) {
+		$xmlCode=simplexml_load_string($strXml,'SimpleXMLElement', LIBXML_NOCDATA);
+		$arrayCode=get_object_vars_final($xmlCode);
+		return $arrayCode ;
+	} else {
+		return '';
+	}
+}
+
+function get_object_vars_final($obj){
+	if(is_object($obj)){
+		$obj=get_object_vars($obj);
+	}
+	if(is_array($obj)){
+		foreach ($obj as $key=>$value){
+			$obj[$key]=get_object_vars_final($value);
+		}
+	}
+	return $obj;
+}
+?>
